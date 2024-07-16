45c75e9825e140828f35ee4ff93c403f157ac01b
Luise Rivaldo <58327615+luiserivaldo@users.noreply.github.com>
2024-07-16 18:56:52 +0100

    Revert "add clickable buttons for demo NPCs"
    This reverts commit e26ca9420613910615cdf71b5b9facc50f9be46f.

--- Assets/Scripts/DemoTaskSlider.cs
+++ Assets/Scripts/DemoTaskSlider.cs
@@ -4,27 +4,28 @@ using System.Collections;
 
 public class DemoTaskSlider : MonoBehaviour
 {
-    public Slider taskProgressSlider; // Weekly Slider UI GameObject
+    public Slider taskProgressSlider; // Reference to the UI slider
     public float totalDuration  = 60f; // Duration in seconds to fill the slider
     public float incrementInterval = 2f;  // Interval in seconds for each increment
-    public Slider npcSliders; 
-    public float totalDurationNPC = 15f;
-    public float incrementIntervalNPC = 0.5f;
 
     void Start()
     {
+        if (taskProgressSlider == null)
+        {
+            Debug.LogError("Slider reference not set.");
+            return;
+        }
+
         // Initialize slider values
         taskProgressSlider.minValue = 0;
         taskProgressSlider.maxValue = 1000; // Set the maximum value of the slider
         taskProgressSlider.value = 0;
 
-        
         // Start the coroutine to fill the slider
-        StartCoroutine(FillWeeklySliderInIncrements());
-        StartCoroutine(FillNPCSliderInIncrements());
+        StartCoroutine(FillSliderInIncrements());
     }
 
-    IEnumerator FillWeeklySliderInIncrements()
+    IEnumerator FillSliderInIncrements()
     {
         int numIncrements = (int)(totalDuration / incrementInterval);
         float incrementValue = taskProgressSlider.maxValue / numIncrements;
@@ -37,20 +38,5 @@ public class DemoTaskSlider : MonoBehaviour
 
         // Ensure the slider value is maxed out at the end
         taskProgressSlider.value = taskProgressSlider.maxValue;
-    }
-
-    IEnumerator FillNPCSliderInIncrements()
-    {
-        int numIncrements = (int)(totalDurationNPC / incrementIntervalNPC);
-        float incrementValue = npcSliders.maxValue / numIncrements;
-
-        for (int i = 0; i < numIncrements; i++)
-        {
-            npcSliders.value += incrementValue;
-            yield return new WaitForSeconds(incrementIntervalNPC);  // Wait for 2 seconds before the next increment
-        }
-
-        // Ensure the slider value is maxed out at the end
-        npcSliders.value = npcSliders.maxValue;
     }
 }
